086ae785639cd18eb289dbf140ce7d53778a3131
Shubham Nishad <shubham.nishad@mitel.com>
2020-12-07 00:25:30 -0500

    restructure data

---
+++ static/data.js
@@ -0,0 +1,53 @@
+var date = '2020-12-06';
+
+var confirmed_total = 297046;
+
+var confirmed_deaths = 10981;
+
+var confirmed_recover = 235388;
+
+var total ={
+      "CA-AB": 37312,
+      "CA-BC": 20985,
+      "CA-MB": 10947,
+      "CA-NB": 367,
+      "CA-NL": 303,
+      "CA-NS": 1144,
+      "CA-NU": 18,
+      "CA-NT": 15,
+      "CA-ON": 97010,
+      "CA-PE": 68,
+      "CA-QC": 123853,
+      "CA-SK": 5000,
+      "CA-YT": 24
+}
+var deaths ={
+      "CA-AB": 407,
+      "CA-BC": 290,
+      "CA-MB": 162,
+      "CA-NB": 6,
+      "CA-NL": 4,
+      "CA-NS": 65,
+      "CA-NU": 0,
+      "CA-NT": 0,
+      "CA-ON": 3389,
+      "CA-PE": 0,
+      "CA-QC": 6626,
+      "CA-SK": 31,
+      "CA-YT": 1
+}
+var recover ={
+      "CA-AB": 28321,
+      "CA-BC": 14901,
+      "CA-MB": 4070,
+      "CA-NB": 339,
+      "CA-NL": 289,
+      "CA-NS": 1058,
+      "CA-NU": 0,
+      "CA-NT": 10,
+      "CA-ON": 78303,
+      "CA-PE": 64,
+      "CA-QC": 104848,
+      "CA-SK": 3163,
+      "CA-YT": 22
+}
