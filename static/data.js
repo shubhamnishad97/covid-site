4db6ef817b541fdaa8cd6d6c4437f2460b0c9e36
Shubham Nishad <shubhamnishad25@gmail.com>
2021-03-06 00:14:52 -0500

    add model comparison page

--- static/data.js
+++ static/data.js
@@ -1,70 +1,117 @@
-var date = '2021-03-04';
+var date = '2021-03-05';
 
-var confirmed_total = 883354;
+var confirmed_total = 886600;
 
-var confirmed_deaths = 22152;
+var confirmed_deaths = 22187;
 
-var confirmed_recover = 826328;
+var confirmed_recover = 829415;
 
-var confirmed_vaccines = 558146;
+var confirmed_vaccines = 561238;
 
 var total ={
-      "CA-AB": 134785,
-      "CA-BC": 82473,
-      "CA-MB": 32051,
-      "CA-NB": 1443,
-      "CA-NL": 1002,
-      "CA-NS": 1649,
-      "CA-NU": 369,
+      "CA-AB": 135196,
+      "CA-BC": 83107,
+      "CA-MB": 32104,
+      "CA-NB": 1447,
+      "CA-NL": 1003,
+      "CA-NS": 1651,
+      "CA-NU": 373,
       "CA-NT": 47,
-      "CA-ON": 309728,
-      "CA-PE": 138,
-      "CA-QC": 290377,
-      "CA-SK": 29220,
+      "CA-ON": 310854,
+      "CA-PE": 139,
+      "CA-QC": 291175,
+      "CA-SK": 29432,
       "CA-YT": 72
 }
 var deaths ={
-      "CA-AB": 1911,
-      "CA-BC": 1376,
-      "CA-MB": 903,
+      "CA-AB": 1913,
+      "CA-BC": 1380,
+      "CA-MB": 904,
       "CA-NB": 28,
       "CA-NL": 6,
       "CA-NS": 65,
       "CA-NU": 2,
       "CA-NT": 0,
-      "CA-ON": 7024,
+      "CA-ON": 7040,
       "CA-PE": 0,
-      "CA-QC": 10445,
-      "CA-SK": 391,
+      "CA-QC": 10455,
+      "CA-SK": 393,
       "CA-YT": 1
 }
 var recover ={
-      "CA-AB": 128261,
-      "CA-BC": 76289,
-      "CA-MB": 30005,
-      "CA-NB": 1378,
-      "CA-NL": 871,
+      "CA-AB": 128644,
+      "CA-BC": 76752,
+      "CA-MB": 30067,
+      "CA-NB": 1385,
+      "CA-NL": 880,
       "CA-NS": 1555,
-      "CA-NU": 354,
+      "CA-NU": 355,
       "CA-NT": 46,
-      "CA-ON": 287424,
-      "CA-PE": 114,
-      "CA-QC": 272553,
-      "CA-SK": 27407,
+      "CA-ON": 288583,
+      "CA-PE": 115,
+      "CA-QC": 273430,
+      "CA-SK": 27532,
       "CA-YT": 71
 }
 var vaccines ={
-      "CA-AB": 89786,
-      "CA-BC": 86746,
-      "CA-MB": 29583,
+      "CA-AB": 90486,
+      "CA-BC": 86865,
+      "CA-MB": 29847,
       "CA-NB": 12142,
       "CA-NL": 8427,
-      "CA-NS": 14219,
-      "CA-NU": 5125,
+      "CA-NS": 14395,
+      "CA-NU": 5144,
       "CA-NT": 4558,
-      "CA-ON": 268118,
+      "CA-ON": 269063,
       "CA-PE": 5273,
       "CA-QC": 0,
-      "CA-SK": 27812,
-      "CA-YT": 6357
+      "CA-SK": 27945,
+      "CA-YT": 7093
+}
+var active_cases ={
+      "CA-AB": 4639,
+      "CA-BC": 4975,
+      "CA-MB": 1133,
+      "CA-NB": 34,
+      "CA-NL": 117,
+      "CA-NS": 31,
+      "CA-NU": 16,
+      "CA-NT": 1,
+      "CA-ON": 15231,
+      "CA-PE": 24,
+      "CA-QC": 7290,
+      "CA-SK": 1507,
+      "CA-YT": 0
+}
+var prev10json ={
+      "CA-AB": 3555,
+      "CA-BC": 4797,
+      "CA-MB": 1202,
+      "CA-ON": 11930,
+      "CA-QC": 21380,
+      "CA-SK": 1904
+}
+var prev20json ={
+      "CA-AB": 3130,
+      "CA-BC": 4470,
+      "CA-MB": 1523,
+      "CA-ON": 10858,
+      "CA-QC": 50094,
+      "CA-SK": 2352
+}
+var diffprev10json ={
+      "CA-AB": -1084,
+      "CA-BC": -178,
+      "CA-MB": 69,
+      "CA-ON": -3301,
+      "CA-QC": 14090,
+      "CA-SK": 397
+}
+var diffprev20json ={
+      "CA-AB": -1509,
+      "CA-BC": -505,
+      "CA-MB": 390,
+      "CA-ON": -4373,
+      "CA-QC": 42804,
+      "CA-SK": 845
 }
